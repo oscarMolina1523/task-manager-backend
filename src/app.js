1088edbff65df060eca0ea13fc7c390199a6ec5e
oscarMolina1523 <oscarmolina1523@gmail.com>
2024-12-28 18:07:34 -0600

    adding cors to use in the frontend

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
+import cors from 'cors';
 import express from 'express';
 import taskRoutes from './routes/taskRoutes.js';
 import setupSwagger from './utils/swagger.js';
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
